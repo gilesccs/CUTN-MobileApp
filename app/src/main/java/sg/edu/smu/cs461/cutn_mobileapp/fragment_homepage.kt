0f6bb29976fb28d0b111833961a887c8638b50a4
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-03-19 16:15:39 +0800

    changed images

--- app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/fragment_homepage.kt
+++ app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/fragment_homepage.kt
@@ -1,13 +1,19 @@
 package sg.edu.smu.cs461.cutn_mobileapp
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class fragment_homepage : Fragment(), View.OnClickListener {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,7 +27,13 @@ class fragment_homepage : Fragment(), View.OnClickListener {
         // code to be written here
     }
 
-    override fun onClick(p0: View?) {
+    override fun onClick(v: View?) {
         // code to be written here
+        val button = v as ImageButton
+        val tag = button.tag.toString()
+
+//        val myIntent = Intent(activity, DetailsActivity::class.java)
+//        myIntent.putExtra("pokemon_name", tag)
+//        startActivity(myIntent)
     }
 }
