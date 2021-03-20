5da8a11eb3635397f1c1ed5617ae519824fa6b55
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2021-03-20 12:25:14 +0800

    for androidx

--- app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/AllProducts.kt
+++ app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/AllProducts.kt
@@ -1,11 +1,53 @@
 package sg.edu.smu.cs461.cutn_mobileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
 
 class AllProducts : AppCompatActivity() {
+
+    private lateinit var productAdapter: ProductAdapter
+    private var products = listOf<Product>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_all_products)
+
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val swipeRefreshLayout = findViewById<androidx.swiperefreshlayout>(R.id.swipeRefreshLayout)
+        swipeRefreshLayout.setColorSchemeColors(ContextCompat.getColor(this, R.color.colorPrimary))
+
+        swipeRefreshLayout.isRefreshing = true
+
+        // assign a layout manager to the recycler view
+        products_recyclerview.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+        getProducts()
+
     }
+
+
+    fun getProducts() {
+
+        swipeRefreshLayout.isRefreshing = false
+//        products = response.body()!!
+        val myDBHelper = MyDBHelper(this)
+        products = myDBHelper.readByCategory()
+
+        productAdapter = ProductAdapter(this, products)
+
+        products_recyclerview.adapter = productAdapter
+        productAdapter.notifyDataSetChanged()
+
+
+    }
+}
 }
---
+++ app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/ProductAdapter.kt
@@ -0,0 +1,38 @@
+package sg.edu.smu.cs461.cutn_mobileapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductAdapter(var context: Context, var products: List<Product> = arrayListOf()) :
+    RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ProductAdapter.ViewHolder {
+        // The layout design used for each list item
+        val view = LayoutInflater.from(context).inflate(R.layout.product_row_item, null)
+        return ViewHolder(view)
+
+    }
+    // This returns the size of the list.
+    override fun getItemCount(): Int = products.size
+
+    override fun onBindViewHolder(viewHolder: ProductAdapter.ViewHolder, position: Int) {
+        //we simply call the `bindProduct` function here
+        viewHolder.bindProduct(products[position])
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        // This displays the product information for each item
+        fun bindProduct(product: Product) {
+
+            itemView.product_name.text = product.name
+            itemView.product_price.text = "$${product.price.toString()}"
+//            Picasso.get().load(product.photos[0].filename).fit().into(itemView.product_image)
+
+        }
+
+    }
+
+}
