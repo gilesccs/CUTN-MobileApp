1df1a3b5fa41f9070fb07ea7b8c52547b4d88a14
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-03-20 18:03:23 +0800

    changed UI

--- app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/ProductAdapter.kt
+++ app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/ProductAdapter.kt
@@ -16,10 +16,10 @@ class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adap
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val currentProduct = productList[position]
-        holder.productName.text = currentProduct.productname
-        holder.price.text = "$" + currentProduct.price.toString()
-        holder.quantity.text = currentProduct.quantity
-        holder.description.text = currentProduct.description
+        holder.productName.text = "Item: " + currentProduct.productname
+        holder.description.text = "Description: " + currentProduct.description
+        holder.price.text = "Price: $" + currentProduct.price.toString()
+        holder.quantity.text = "Quantity: " + currentProduct.quantity
     }
 
     override fun getItemCount(): Int {
@@ -29,8 +29,8 @@ class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adap
     class ProductViewHolder(productView: View) : RecyclerView.ViewHolder(productView) {
 //        val imageView: ImageView = productView.findViewById(R.id.imageView)
         val productName: TextView = productView.findViewById(R.id.productName)
+        val description: TextView = productView.findViewById(R.id.description)
         val price: TextView = productView.findViewById(R.id.price)
         val quantity: TextView = productView.findViewById(R.id.quantity)
-        val description: TextView = productView.findViewById(R.id.description)
     }
 }
