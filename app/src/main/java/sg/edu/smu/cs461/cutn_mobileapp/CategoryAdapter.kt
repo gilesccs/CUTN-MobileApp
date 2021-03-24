d994a12a8d2f25827332280aa80b239854fb451c
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-03-24 16:22:52 +0800

    changes to the cards

--- app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/CategoryAdapter.kt
+++ app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/CategoryAdapter.kt
@@ -1,10 +1,12 @@
 package sg.edu.smu.cs461.cutn_mobileapp
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
 class CategoryAdapter (private val productList: List<Category>) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>(){
@@ -17,6 +19,15 @@ class CategoryAdapter (private val productList: List<Category>) : RecyclerView.A
         val currentProduct = productList[position]
         holder.categoryName.text = currentProduct.category
         holder.imageViewCategory.setImageResource(currentProduct.imageResource)
+        if (position == 0){
+            holder.cardView.setCardBackgroundColor(Color.rgb(240,143,143))
+        } else if (position == 1){
+            holder.cardView.setCardBackgroundColor(Color.rgb(153,255,102))
+        } else if (position == 2){
+            holder.cardView.setCardBackgroundColor(Color.rgb(245,245,245))
+        } else {
+            holder.cardView.setCardBackgroundColor(Color.rgb(255,255,102))
+        }
     }
 
     override fun getItemCount(): Int {
@@ -26,5 +37,6 @@ class CategoryAdapter (private val productList: List<Category>) : RecyclerView.A
     class CategoryViewHolder(categoryView: View) : RecyclerView.ViewHolder(categoryView) {
         val imageViewCategory: ImageView = categoryView.findViewById(R.id.imageViewCategory)
         val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+        val cardView: CardView = categoryView.findViewById(R.id.cardView)
     }
 }
