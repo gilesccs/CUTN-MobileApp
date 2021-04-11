2510aca145f8e1c88b7ae2b8617ac086d9bf1ddc
gilesccs <54528035+gilesccs@users.noreply.github.com>
2021-04-11 22:05:37 +0800

    Added voice recognition

--- app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/MainActivity.kt
+++ app/src/main/java/sg/edu/smu/cs461/cutn_mobileapp/MainActivity.kt
@@ -1,23 +1,32 @@
 package sg.edu.smu.cs461.cutn_mobileapp
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.util.Log
 import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private var REQ_CODE = 3213
     private lateinit var gotoRewards: ImageView
+    private var SPEECH_CODE = 1999
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val micBtn = findViewById<ImageButton>(R.id.microphone)
+        micBtn?.setOnClickListener{
+            voiceInput()
+        }
         individualPage()
 
         val productList = generateDummyListForPopularItem(5)
@@ -31,6 +40,18 @@ class MainActivity : AppCompatActivity() {
         recyclerViewCategory.adapter = CategoryAdapter(categoryList)
         recyclerViewCategory.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
         recyclerViewCategory.setHasFixedSize(true)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i("test",requestCode.toString())
+        if(requestCode == SPEECH_CODE){
+            Log.i("test","yup")
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val searchBtn = findViewById<EditText>(R.id.searchForProduct)
+            Log.i("test","result is " + result?.get(0).toString())
+            searchBtn.setText(result?.get(0).toString())
+        }
     }
 
     private fun generateDummyListForPopularItem(size: Int): List<PopularItem>{
@@ -83,10 +104,25 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun individualPage() {
-        val rewardsBtn = findViewById<ImageButton>(R.id.microphone)
+        val rewardsBtn = findViewById<ImageButton>(R.id.photo)
         rewardsBtn?.setOnClickListener{
             val it = Intent(this, IndividualProduct::class.java)
             startActivityForResult(it, 4321)
         }
     }
+
+    private fun voiceInput() {
+        if(!SpeechRecognizer.isRecognitionAvailable(this)){
+            Toast.makeText(this, "Speech Recognition is not available on this device!", Toast.LENGTH_SHORT).show()
+        }else{
+            val i = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT,"Name a grocery!")
+            Log.i("test","reached")
+            startActivityForResult(i,SPEECH_CODE)
+        }
+    }
+
+
 }
